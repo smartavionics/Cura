428a4611e381f10d9a0d47048031b7f7ef9543dc
Mark Burton <markb@smartavionics.com>
2020-06-07 18:36:42 +0100

    Don't backface cull the lower layers when using pi4 shaders.
    As we don't close the ends, the line tubes look better if you can see the insides.

--- plugins/SimulationView/SimulationPass.py
+++ plugins/SimulationView/SimulationPass.py
@@ -220,7 +220,7 @@ class SimulationPass(RenderPass):
 
                     # for the PI 4, only bother to output the lower layers using the shadow shader when riding the nozzle
                     if not self._pi4_shaders or self._current_shader != self._layer_shadow_shader or ride_the_nozzle:
-                        layers_batch = RenderBatch(self._current_shader, type = RenderBatch.RenderType.Solid, mode = RenderBatch.RenderMode.Lines, range = (start, end), backface_cull = True)
+                        layers_batch = RenderBatch(self._current_shader, type = RenderBatch.RenderType.Solid, mode = RenderBatch.RenderMode.Lines, range = (start, end), backface_cull = not self._pi4_shaders)
                         layers_batch.addItem(node.getWorldTransformation(), layer_data)
                         layers_batch.render(self._scene.getActiveCamera())
 
