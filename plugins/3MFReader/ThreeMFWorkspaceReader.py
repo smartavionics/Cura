32b928f7f50a4c8c2696a513808385dd3e13be62
Mark Burton <burtoogle@gmail.com>
2020-07-14 07:17:27 +0100

    Avoid crash if variant is unknown.

--- plugins/3MFReader/ThreeMFWorkspaceReader.py
+++ plugins/3MFReader/ThreeMFWorkspaceReader.py
@@ -1022,7 +1022,10 @@ class ThreeMFWorkspaceReader(WorkspaceReader):
                 node = machine_node.variants.get(machine_node.preferred_variant_name, next(iter(machine_node.variants.values())))
             else:
                 variant_name = extruder_info.variant_info.parser["general"]["name"]
-                node = ContainerTree.getInstance().machines[global_stack.definition.getId()].variants[variant_name]
+                if variant_name in ContainerTree.getInstance().machines[global_stack.definition.getId()].variants:
+                    node = ContainerTree.getInstance().machines[global_stack.definition.getId()].variants[variant_name]
+                else:
+                    node = machine_node.variants.get(machine_node.preferred_variant_name, next(iter(machine_node.variants.values())))
             extruder_stack.variant = node.container
 
     def _applyMaterials(self, global_stack, extruder_stack_dict):
